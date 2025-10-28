453e41f3130f4c8c8b6b5752556a94cb37190a8e
Matías Vespa <30608973+Ranger9707@users.noreply.github.com>
2025-10-28 16:44:33 -0300

    Remove unnecessary imports and clean up code

--- src/db/reservasDeServicios.js
+++ src/db/reservasDeServicios.js
@@ -27,15 +27,12 @@ export default class ReservasServicios {
         try {
             await conexion.beginTransaction();
 
-            // 1. Borrar servicios anteriores
             const sqlDelete = 'DELETE FROM reservas_servicios WHERE reserva_id = ?';
             await conexion.execute(sqlDelete, [reserva_id]);
 
-            // 2. Insertar servicios nuevos
             for (const servicio of servicios){
                 const sqlInsert = `INSERT INTO reservas_servicios (reserva_id, servicio_id, importe) 
                     VALUES (?,?,?);`;
-                // Se usa 'await' aquí
                 await conexion.execute(sqlInsert, [reserva_id, servicio.servicio_id, servicio.importe ]);
             }
 
@@ -48,4 +45,5 @@ export default class ReservasServicios {
             return false;
         }
     }
+
 }
