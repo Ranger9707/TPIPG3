a873daa16f6b7754eeff0574b58ac0da3b3a3f68
Matías Vespa <30608973+Ranger9707@users.noreply.github.com>
2025-10-28 16:48:55 -0300

    Add files via upload

--- src/appReservas.js
+++ src/appReservas.js
@@ -1,22 +1,34 @@
 import express from 'express';
-//import handlebars from 'handlebars';
-//import nodemailer from 'nodemailer';
-//import {fileURLToPath} from 'url';
-//import { readFile } from 'fs/promises';
-//import path from 'path';
-//import { conexion } from './db/conexion.js';
+import passport from 'passport';
+import morgan from 'morgan';
+import fs from 'fs';
 
-import {router as SalonesRutasV1} from './v1/routes/salonesRutas.js';
+import { router as SalonesRutasV1} from './v1/routes/salonesRutas.js';
+import { router as ReservasRutasV1} from './v1/routes/reservasRutas.js';
+import { router as AuthRutaV1} from './v1/routes/authRutas.js';
+import { router as ServiciosRutasV1} from './v1/routes/serviciosRutas.js';
+import { router as TurnosRutasV1} from './v1/routes/turnosRutas.js';
+
+import {strategy, validation} from './config/passport.js';
+
 
 const app = express();
+
 app.use(express.json());
-app.use('/api/v1/salones', SalonesRutasV1);
+passport.use(strategy);
+passport.use(validation);
+app.use(passport.initialize());
+
+let log = fs.createWriteStream('./logs/access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: log}));
+app.use(morgan('combined'))
+
+app.use('/api/v1/auth', AuthRutaV1);
+app.use('/api/v1/salones', passport.authenticate('jwt', { session: false }), SalonesRutasV1);
+app.use('/api/v1/reservas', passport.authenticate( 'jwt', { session:false }), ReservasRutasV1);
+app.use('/api/v1/servicios', passport.authenticate('jwt', { session: false }), ServiciosRutasV1);
+app.use('/api/v1/turnos', passport.authenticate('jwt', { session: false }), TurnosRutasV1);
 
 
-
-process.loadEnvFile();
-app.listen(process.env.PUERTO, () => {
-    console.log(`Server esta corriendo en el puerto ${process.env.PUERTO}`);
-});
-
+export default app;
 
