97503c09828bc7efe3034526143fb0bb1f954b1f
Matías Vespa <30608973+Ranger9707@users.noreply.github.com>
2025-10-28 16:55:01 -0300

    Delete src directory

--- src/appServidor.js
+++
@@ -1,7 +0,0 @@
-import app from "./appReservas.js";
-
-process.loadEnvFile();
-
-app.listen(process.env.PUERTO, () => {
-    console.log(`Server esta corriendo en el puerto ${process.env.PUERTO}`);
-});
--- src/middlewares/autorizarUsuarios.js
+++
@@ -1,8 +0,0 @@
-export default function autorizarUsuarios(tiposPermitidos = []) {
-    return (req, res, next) => {
-        const usuario = req.usuario;
-        if (!usuario || !tiposPermitidos.includes(usuario.tipo_usuario)) {
-            return res.status(403).json({ mensaje: 'Acceso no autorizado' });
-        }
-        next();
-    };}
