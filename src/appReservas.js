e57ce03b60c9057b42b0d9cc56a5ecb672b76f70
Matías Vespa <30608973+Ranger9707@users.noreply.github.com>
2025-11-05 15:48:57 -0300

    Add files via upload
    SWAGGER AUTOGEN

--- src/appReservas.js
+++ src/appReservas.js
@@ -3,6 +3,10 @@ import path from 'path';
 import passport from 'passport';
 import morgan from 'morgan';
 import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+
+import { readFileSync } from 'fs';
+const swaggerFile = JSON.parse(readFileSync('./swagger-output.json'));
 
 import { router as SalonesRutasV1} from './v1/routes/salonesRutas.js';
 import { router as ReservasRutasV1} from './v1/routes/reservasRutas.js';
@@ -12,7 +16,6 @@ import { router as TurnosRutasV1} from './v1/routes/turnosRutas.js';
 import { router as UsuariosRutasV1} from './v1/routes/usuariosRutas.js';
 
 import {strategy, validation} from './config/passport.js';
-
 
 const app = express();
 
@@ -26,9 +29,10 @@ if (!fs.existsSync(logDirectory)) {
     fs.mkdirSync(logDirectory, { recursive: true });
 }
 let log = fs.createWriteStream(path.join(logDirectory, 'access.log'), {flags: 'a'});
-
 app.use(morgan('combined', {stream: log}));
 app.use(morgan('combined'))
+
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/api/v1/auth', AuthRutaV1);
 app.use('/api/v1/salones', passport.authenticate('jwt', { session: false }), SalonesRutasV1);
@@ -37,6 +41,4 @@ app.use('/api/v1/servicios', passport.authenticate('jwt', { session: false }), S
 app.use('/api/v1/turnos', passport.authenticate('jwt', { session: false }), TurnosRutasV1);
 app.use('/api/v1/usuarios', passport.authenticate('jwt', { session: false }), UsuariosRutasV1);
 
-
 export default app;
-
